1149fd964ac19c47071bcc017b528047ec74f8c2
Dzulfan Nur Fachrudin <90804983+Zulpann@users.noreply.github.com>
2023-02-26 11:09:36 -0800

    UPDATE BOSS

--- src/components/topsong.js
+++ src/components/topsong.js
@@ -1,7 +1,35 @@
+import Card1 from "../asset/img/bg-card1.png";
+import Card2 from "../asset/img/bg-card2.png";
+import Card3 from "../asset/img/bg-card3.png";
+import Card4 from "../asset/img/bg-card4.png";
+
 function Topsong() {
     return(
-        <div>
-
+        <div className="body-topsong">
+            <div className="container-topsong">
+            <div className="nav-topsong">
+                <h2>Top Song Barat</h2>
+                <a href="#">See All</a>
+            </div>
+                <div className="list-topsong">
+                    <div className="card-topsong">
+                        <img src={Card1} className="img-topsong" ></img>
+                        <p className="paraf-topsong" >Seni</p>
+                    </div>
+                    <div className="card-topsong">
+                        <img src={Card2} className="img-topsong" ></img>
+                        <p className="paraf-topsong" >E-Sport</p>
+                    </div>
+                    <div className="card-topsong">
+                        <img src={Card3} className="img-topsong" ></img>
+                        <p className="paraf-topsong" >Berita</p>
+                    </div>
+                    <div className="card-topsong">
+                        <img src={Card4} className="img-topsong" ></img>
+                        <p className="paraf-topsong" >Barat</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
